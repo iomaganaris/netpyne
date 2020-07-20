c768b5699c846e7fb49cf0a06c224a8ba98ad8ae
Ioannis Magkanaris <ioannis.magkanaris@epfl.ch>
2020-07-20 12:43:50 +0200

    Added printing out_neuron.dat to compare spikes

--- examples/coreneuron/m1/init.py
+++ examples/coreneuron/m1/init.py
@@ -20,3 +20,8 @@ from netParams import netParams
 
 print("Starting sim ...")
 sim.createSimulateAnalyze(netParams, cfg)
+
+if sim.rank == 0:
+    with open('out_neuron.dat', 'w') as f:
+        for spkid, spkt in zip(sim.allSimData['spkid'], sim.allSimData['spkt']):
+            f.write('%.8g\t%d\n' % (spkt, spkid))
